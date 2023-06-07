b38efd83bb36d4cd3a7a2cf97b8a0ff5071c3d87
x-mas87 <dlkst@naver.com>
2023-06-07 16:24:52 +0900

    first commit

---
+++ src/main/java/com/example/restaurant/wishlist/entity/WishListEntity.java
@@ -0,0 +1,4 @@
+package com.example.restaurant.wishlist;
+
+public class WishListEntity {
+}
---
+++ src/main/java/com/example/restaurant/wishlist/entity/repository/WishListRepository.java
@@ -0,0 +1,2 @@
+package com.example.restaurant.wishlist.entity.repository;public class WishListRepository {
+}
